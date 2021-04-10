5ddc5c86389bf8e904c6e46e0378027509c5c6a1
KasunW-UoM <kasun.18@itfac.mrt.ac.lk>
2021-04-10 21:36:59 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,114 @@
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Manufacturing and Creating ",
+    img: "img/pro-5.jpg",
+    price: 2500,
+    company: "oncall System",
+    info:
+      "Manufacturing covers, as a group of careers, any job that is involved in the fabrication and processing of materials into commercial products. All careers related to the planning, production, producing, and design of end products are included in manufacturing.The subCatogoey are the Boilermakers,Electric Motor, Power Tool, and Related Repairers,Electrical and Electronic Equipment Assemblers,Fabric Menders, Except Garment",
+      
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Hospitality & Tourism",
+    img: "img/pro-6.jpg",
+    price:3000,
+    company: "oncall System",
+    info:
+      "The hospitality career family include every job and profession associated with lodging, dinining, and recreational events. A broad group of professions are represented, including everything from hotel management to tour guides.The subCatogoey are Animal Trainers,Bakers,Chefs and Head Cooks,Food Service Managers and Slot Supervisors",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Health Science",
+    img: "img/pro-7.jpg",
+    price: 16000,
+    company: "oncall System",
+    info:
+      "The health sciences career group covers a diverse range of professions, ranging from those directly involved with patients, to those that support, manufacture, and produce medical and health related products and innovationsand subcatogery is Acupuncturists ,Biologists ,Biomedical Engineers,Chiropractors,Clinical Data Managers ,Midwives and Surgeons.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Finance Services",
+    img: "img/pro-8.jpg",
+    price: 6000,
+    company: "oncall System",
+    info:
+      "The financial career group covers all positions where money and finance management are the primary tasks. This includes not only finance positions in investment banking and brokerage, but includes all banking and insurance sectors. Covering all aspects of financial management, planning, and risk mitigation, the finance career group has been a rapidly growing and lucrative field. In addition the entire banking, insurance, and financial industry, this group of jobs also includes financial management within regular business.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Education  Teching and Training  Service",
+    img: "img/pro-1.jpg",
+    price: 2000,
+    company: "oncall System",
+    info:
+      "Careers in education and training make up a massive volume of jobs and a good percentage of total employment in the country. This career family includes all teaching and academic administration positions across all levels of education. This broad career category also includes corporate and professional eudcational, including but not limited to, training, user implementation instruction, corporate motivation and seminars, and other related jobs.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Architecture and Construction",
+    img: "img/pro-2.jpg",
+    price: 8000,
+    company: "oncall System",
+    info:
+      "The group of careers encompassing construction and architecture is a rapidly advancing field of study. Professionals in the architecture and construction fields develop and plan structural components, infrastructure, transportation systems. Additionally to design work, this field of study encompasses repairs and restorations of existing structures, be they buildings, highways, bridges, or other man made objects.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Human and  resouse Services ",
+    img: "img/pro-4.jpg",
+    price: 5000,
+    company: "oncall System",
+    info:
+      "The human services career group covers all employment related to helping meet the needs of individuals and families. Helping people is the major goal of this career group, and in our modern society, these postiions play a very important role in the delivery of services and in the strengthening of communities.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Transportation, Distribution and Logistics",
+    img: "img/pro-3.jpg",
+    price: 20000,
+    company: "oncall System",
+    info:
+      "The career group of transportation includes every job which involves moving people or things from one place to another. Transportation professionals plan, manage, and execute the movement of products, goods, commodities, and people through a variety of means. Covering water, rail, road, and air transportation, this career group spans multiple industries.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Transportation, Distribution and Logistics ",
+  img: "img/pro-3.jpg",
+  price: 20000,
+  company: "oncall System",
+  info:
+    "The career group of transportation includes every job which involves moving people or things from one place to another. Transportation professionals plan, manage, and execute the movement of products, goods, commodities, and people through a variety of means. Covering water, rail, road, and air transportation, this career group spans multiple industries.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
+
